f36f6a928a7f8f00a9ca9dc931218e0994c33178
reidsvilleneuve <reidsvilleneuve@gmail.com>
2014-11-20 11:59:14 -0700

    Added Cat and Cat AI

--- CheeseFinder/CheeseFinder/Program.cs
+++ CheeseFinder/CheeseFinder/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace CheeseFinder
 {
@@ -10,6 +11,7 @@ namespace CheeseFinder
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             CheeseFinder game = new CheeseFinder();
             game.PlayGame();
         }
@@ -20,12 +22,15 @@ namespace CheeseFinder
     /// </summary>
     class CheeseFinder
     {
-        //For quick setting of options.
-        const bool _showGrid = false;
-        const bool _placeWalls = true;
-        const int _gridXNodes = 10;
-        const int _gridYNodes = 20;
-        const int _wallChance = 15;
+        
+        //-------------------------------
+        const bool _showGrid = false;  //
+        const bool _placeWalls = true; //
+        const int _gridXNodes = 10;    //For quick setting of options.
+        const int _gridYNodes = 20;    //
+        const int _wallChance = 5;    //
+        //-------------------------------
+
 
         private Point[,] _grid = new Point[_gridXNodes, _gridYNodes];
         public Point[,] Grid
@@ -34,8 +39,25 @@ namespace CheeseFinder
             set { _grid = value; }
         }
 
-        public Point Mouse { get; set; }
-        public Point Cheese { get; set; }
+        private Point _mouse;
+        public Point Mouse 
+        {
+            get { return _mouse; }
+            set { _mouse = value; } 
+        }
+
+        private Point _cheese;
+        public Point Cheese
+        {
+            get { return _cheese; }
+            set { _cheese = value; }
+        }
+        private List<Point> _cats = new List<Point>();
+        public List<Point> Cats
+        {
+            get { return _cats; }
+            set { _cats = value; }
+        }
 
 
         // ---- Constructors
@@ -47,8 +69,8 @@ namespace CheeseFinder
         public CheeseFinder()
         {
             Random rng = new Random();
-            int randX = rng.Next(0, Grid.GetUpperBound(0) + 1); //GridUpperBound is the length of the array.
-            int randY = rng.Next(0, Grid.GetUpperBound(1) + 1); //0 for x, 1 for y.
+            int randX = rng.Next(Grid.GetUpperBound(0) + 1); //GridUpperBound is the length of the array.
+            int randY = rng.Next(Grid.GetUpperBound(1) + 1); //0 for x, 1 for y.
 
             //Create a new point at each node of Grid
             for (int y = 0; y < Grid.GetUpperBound(1) + 1; y++)
@@ -58,38 +80,99 @@ namespace CheeseFinder
                     //Randomly place a wall at this point if const is true. This can possibly block the cheese in -
                     //Todo: attempt to ensure that this won't happen.
                     if(_placeWalls)
-                        if (rng.Next(0, 101) < _wallChance)
+                        if (rng.Next(101) < _wallChance)
                             Grid[x, y].Contains = PointContents.Wall;
                 }
+
             //Position the Mouse at a random point on the field
+            PlaceMouse();
+            //Position the Cheese at a random point on the field.
+            PlaceCheese();
+            //Positon the Cat at a random point on the field.
+            PlaceCat();
+
+
+        }
+
+
+        // ---- Methods
+
+
+        /// <summary>
+        /// Places the Mouse in the Grid.
+        /// </summary>
+        public void PlaceMouse()
+        {
+            Random rng = new Random();
+            int randX;
+            int randY;
+            bool placed = false;
+            
             do
             {
-                randX = rng.Next(0, Grid.GetUpperBound(0) + 1);
-                randY = rng.Next(0, Grid.GetUpperBound(1) + 1);
+                randX = rng.Next(Grid.GetUpperBound(0) + 1);
+                randY = rng.Next(Grid.GetUpperBound(1) + 1);
 
                 if (Grid[randX, randY].CanMoveTo) //Place is there is no wall in the way.
                 {
                     Grid[randX, randY].Contains = PointContents.Mouse;
                     this.Mouse = new Point(randX, randY);
+                    placed = true;
                 }
-            } while (!(Grid[randX, randY].Contains == PointContents.Mouse)); //Loop until mouse is placed.
+            } while (!placed); //Loop until mouse is placed.
 
-            //Position the Cheese at a random point on the field.
+        }
+
+        /// <summary>
+        /// Places the Cheese in the Grid.
+        /// </summary>
+        public void PlaceCheese()
+        {
+            Random rng = new Random();
+            int randX;
+            int randY;
+            bool placed = false;
+            
             do
             {
-                randX = rng.Next(0, Grid.GetUpperBound(0) + 1);
-                randY = rng.Next(0, Grid.GetUpperBound(1) + 1);
+                randX = rng.Next(Grid.GetUpperBound(0) + 1);
+                randY = rng.Next(Grid.GetUpperBound(1) + 1);
 
                 if (Grid[randX, randY].CanMoveTo) // Place if there is no wall or mouse in the way.
                 {
                     Grid[randX, randY].Contains = PointContents.Cheese;
                     this.Cheese = new Point(randX, randY);
+                    placed = true;
                 }
-            } while (!Grid[randX, randY].CanMoveTo); //Loop until cheese is placed.
+            } while (!placed); //Loop until cheese is placed.
+
         }
 
+        /// <summary>
+        /// Adds a cat in the Field.
+        /// </summary>
+        public void PlaceCat()
+        {
+            Random rng = new Random();
+            int randX;
+            int randY;
+            bool placed = false;
 
-        // ---- Methods
+            do
+            {
+
+                randX = rng.Next(Grid.GetUpperBound(0) + 1);
+                randY = rng.Next(Grid.GetUpperBound(1) + 1);
+
+                if (Grid[randX, randY].CanMoveTo && !Grid[randX, randY].CatAvoids) // Place if there is no wall, 
+                {                                                                  // mouse, OR cheese in the way.
+                    Grid[randX, randY].Contains = PointContents.Cat;
+                    this.Cats.Add(new Point(randX, randY));
+                    placed = true;
+                }
+            } while (!placed); //Loop until cat is placed.
+
+        }
         
         /// <summary>
         /// Writes the game field to the console.
@@ -101,18 +184,29 @@ namespace CheeseFinder
             {
                 if (y == 0)
                 {
+                    //Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
                     //Draw the border. 
-                    Console.Write(" "); //The upper left corner.
+                    //for (int i = 0; i < (Console.WindowHeight / 4); i++ )
+                        Console.Write("\n");
+
+                    for (int i = 0; i < (Console.WindowWidth / 4) - (_gridXNodes / 2) + 1; i++)
+                        Console.Write(" "); //Keep grid centered - one space for every 4 window width units.
+
                     for (int i = 0; i < Grid.GetUpperBound(0) + 1; i++)
                         Console.Write("---"); //Top border. Three dashes per node in the list.
-                    Console.Write(" "); //The upper right corner.
+
+                    Console.Write("           "); //The upper right corner.
                     Console.WriteLine(); //New line before grid is drawn.
                 }
                 for (int x = 0; x < Grid.GetUpperBound(0) + 1; x++)
                 {
                     if (x == 0)
+                    {
+                        for (int i = 0; i < Console.WindowWidth / 4 - (_gridXNodes / 2); i++)
+                            Console.Write(" "); //Keep grid centered - one space for every 4 window width units.
+                        
                         Console.Write("|"); //Border before each row.
-
+                    }
                     //The grid itself.
                     Console.Write(Grid[x, y].DrawPoint(_showGrid));
                     Console.ResetColor(); //In case color was changed by Mouse or Cheese.
@@ -123,10 +217,11 @@ namespace CheeseFinder
                 Console.WriteLine(); //New line before bottom border.
             }
 
-            Console.Write(" "); //Lower-left corner.
+            for (int i = 0; i < (Console.WindowWidth / 4) - (_gridXNodes / 2) + 1; i++)
+                Console.Write(" "); //Keep grid centered - one space for every 4 window width units.
+
             for (int i = 0; i < Grid.GetUpperBound(0) + 1; i++)
                 Console.Write("---"); //Bottom border. Three dashes per node.
-            Console.Write(" "); //Lower-right corner.
         }
 
         /// <summary>
@@ -239,19 +334,96 @@ namespace CheeseFinder
 
         }
 
+        public void MoveCats()
+        {
+            int xDistance;
+            int yDistance;
+            foreach(Point cat in Cats)
+            {
+                xDistance = cat.X - Mouse.X; //Negative if mouse is to the right, positive if to the left.
+                yDistance = cat.Y - Mouse.Y; //Negative is mouse is below, postive if above.
+
+                if (Math.Abs(xDistance) > Math.Abs(yDistance)) //X is greater than y. Move on X plane.
+                {
+                    if (xDistance < 0) //The mouse is to the right.
+                    {
+                        if (!(Grid[cat.X + 1, cat.Y]).CatAvoids)
+                        {
+                            Grid[cat.X + 1, cat.Y].Contains = PointContents.Cat;
+                            Grid[cat.X, cat.Y].Contains = PointContents.Space;
+                            cat.X += 1;
+                        }
+                    }
+                    else //The mouse is to the left.
+                    {
+                        if (!(Grid[cat.X - 1, cat.Y]).CatAvoids)
+                        {
+                            Grid[cat.X - 1, cat.Y].Contains = PointContents.Cat;
+                            Grid[cat.X, cat.Y].Contains = PointContents.Space;
+                            cat.X -= 1;
+                        }
+                    }
+                }
+                else
+                {
+                    //Y is greater than or equal to X. Move on Y plane.
+
+                    //Because of the wide shape of the sprites in this game, there is likely to be more
+                    //Y nodes than X nodes, so we prioritize Y movement.
+
+                    if (yDistance < 0) //The mouse is below.
+                    {
+                        if (!(Grid[cat.X, cat.Y + 1]).CatAvoids)
+                        {
+                            Grid[cat.X, cat.Y + 1].Contains = PointContents.Cat;
+                            Grid[cat.X, cat.Y].Contains = PointContents.Space;
+                            cat.Y += 1;
+                        }
+                    }
+                    else //The mouse is to the above.
+                    {
+                        if (!(Grid[cat.X, cat.Y - 1]).CatAvoids)
+                        {
+                            Grid[cat.X, cat.Y - 1].Contains = PointContents.Cat;
+                            Grid[cat.X, cat.Y].Contains = PointContents.Space;
+                            cat.Y -= 1;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        //public bool CatMoveValid()
+        //{
+
+        //}
+
+
         /// <summary>
         /// Play a game of CheeeseFinder.
         /// </summary>
         public void PlayGame()
         {
             bool foundCheese = false; //Will beset to True when cheese is found.
+            
+            //The following is a timer for multithreading. This feature is not active for this release.
 
+            //System.Timers.Timer gameTimer = new System.Timers.Timer();
+            //gameTimer.Elapsed += new ElapsedEventHandler(OnGameTimer);
+            //gameTimer.Interval = 100;
+            //gameTimer.Enabled = true;
+            //gameTimer.AutoReset = false;
+
+            //Main game loop.
             do
             {
                 Console.Clear();
+                //Draws the game field.
                 this.DrawGrid(_showGrid);
+                
                 foundCheese = MoveMouse(); //Returns true if mouse touches cheese.
-
+                MoveCats();
             } while (!foundCheese);
 
             //Will only exit on victory in this version.
@@ -259,12 +431,27 @@ namespace CheeseFinder
             Console.WriteLine("You win!");
             Console.ReadKey();
         }
+
+        /*
+        /// <summary>
+        /// This will be used for multithreading during a later release. Not active at this time.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        public void OnGameTimer(Object source, ElapsedEventArgs e)
+        {
+            Console.Clear();
+            DrawGrid(_showGrid);
+            System.Timers.Timer theTimer = (System.Timers.Timer)source;
+            theTimer.Enabled = true;
+        }
+        */
     }
 
     /// <summary>
     /// The possible contents of a Point in the CheeseFinder game.
     /// </summary>
-    enum PointContents { Space, Mouse, Cheese, Wall }
+    enum PointContents { Space, Mouse, Cheese, Wall, Cat }
     
 
     /// <summary>
@@ -299,20 +486,29 @@ namespace CheeseFinder
                 switch (value)
                 {
                     case PointContents.Mouse:
-                        _containsChar = 'M';
+                        _containsChar = "M";
                         _canMoveTo = false;
+                        _catAvoids = false;
                         break;
                     case PointContents.Cheese:
-                        _containsChar = 'C';
+                        _containsChar = "C";
                         _canMoveTo = true;
+                        _catAvoids = true;
                         break;
                     case PointContents.Wall:
-                        _containsChar = 'X';
+                        _containsChar = "X";
                         _canMoveTo = false;
+                        _catAvoids = true;
+                        break;
+                    case PointContents.Cat:
+                        _containsChar = "E";
+                        _canMoveTo = false;
+                        _catAvoids = true;
                         break;
                     default:
-                        _containsChar = ' ';
+                        _containsChar = " ";
                         _canMoveTo = true;
+                        _catAvoids = false;
                         break;
                 }
                 
@@ -320,8 +516,8 @@ namespace CheeseFinder
             }
         }
 
-        private char _containsChar;
-        public char ContainsChar
+        private string _containsChar;
+        public string ContainsChar
         {
             get { return _containsChar; }
         }
@@ -330,6 +526,12 @@ namespace CheeseFinder
         public bool CanMoveTo
         {
             get { return _canMoveTo; }
+        }
+
+        private bool _catAvoids;
+        public bool CatAvoids
+        {
+            get { return _catAvoids; }
         }
 
 
@@ -378,6 +580,9 @@ namespace CheeseFinder
 
             else if (this.Contains == PointContents.Cheese)
                 Console.ForegroundColor = ConsoleColor.Yellow;
+
+            else if (this.Contains == PointContents.Cat)
+                Console.ForegroundColor = ConsoleColor.Red;
 
             //If we are to draw the entire grid, return all points with brackets.
             if(drawGrid)
