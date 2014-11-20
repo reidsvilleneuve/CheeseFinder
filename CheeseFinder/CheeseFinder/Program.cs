c59d642e3668591929b4f102ac907d93b51638d8
reidsvilleneuve <reidsvilleneuve@gmail.com>
2014-11-20 00:21:41 -0700

    Completed this iteration of CheeseFinder.

--- CheeseFinder/CheeseFinder/Program.cs
+++ CheeseFinder/CheeseFinder/Program.cs
@@ -15,9 +15,18 @@ namespace CheeseFinder
         }
     }
 
+    /// <summary>
+    /// A game of CheeseFinder. Control the Mouse to find the cheese.
+    /// </summary>
     class CheeseFinder
     {
-        private Point[,] _grid = new Point[10,20];
+        //For quick setting of options.
+        const bool _showGrid = false;
+        const int _gridXNodes = 10;
+        const int _gridYNodes = 20;
+        const int _wallChance = 15;
+
+        private Point[,] _grid = new Point[_gridXNodes, _gridYNodes];
         public Point[,] Grid
         {
             get { return _grid; }
@@ -31,55 +40,103 @@ namespace CheeseFinder
         // ---- Constructors
 
 
+        /// <summary>
+        /// Generates the grid for the game, and randomly places the mouse/cheese.
+        /// </summary>
         public CheeseFinder()
         {
             Random rng = new Random();
             int randX = rng.Next(0, Grid.GetUpperBound(0) + 1);
             int randY = rng.Next(0, Grid.GetUpperBound(1) + 1);
 
+            //Create a new point at each node of Grid
             for (int y = 0; y < Grid.GetUpperBound(1) + 1; y++)
                 for (int x = 0; x < Grid.GetUpperBound(0) + 1; x++)
+                {
                     Grid[x, y] = new Point(x, y);
-
-            Grid[randX, randY].Contains = PointContents.Mouse;
-            this.Mouse = new Point(randX, randY);
-
+                    //Randomly place a wall at this point. This can possibly block the cheese in -
+                    //Todo: attempt to ensure that this won't happen.
+                    if (rng.Next(0, 101) < _wallChance)
+                        Grid[x, y].Contains = PointContents.Wall;
+                }
+            //Position the Mouse at a random point on the field
             do
             {
                 randX = rng.Next(0, Grid.GetUpperBound(0) + 1);
                 randY = rng.Next(0, Grid.GetUpperBound(1) + 1);
 
-                if (Grid[randX, randY].CanMoveTo)
+                if (Grid[randX, randY].CanMoveTo) //Place is there is no wall in the way.
+                {
+                    Grid[randX, randY].Contains = PointContents.Mouse;
+                    this.Mouse = new Point(randX, randY);
+                }
+            } while (!(Grid[randX, randY].Contains == PointContents.Mouse)); //Loop until mouse is placed.
+
+            //Position the Cheese at a random point on the field.
+            do
+            {
+                randX = rng.Next(0, Grid.GetUpperBound(0) + 1);
+                randY = rng.Next(0, Grid.GetUpperBound(1) + 1);
+
+                if (Grid[randX, randY].CanMoveTo) // Place if there is no wall or mouse in the way.
                 {
                     Grid[randX, randY].Contains = PointContents.Cheese;
                     this.Cheese = new Point(randX, randY);
                 }
-            } while (!Grid[randX, randY].CanMoveTo);
+            } while (!Grid[randX, randY].CanMoveTo); //Loop until cheese is placed.
         }
 
 
-        /// ---- Methods
+        // ---- Methods
         
-
+        /// <summary>
+        /// Writes the game field to the console.
+        /// </summary>
+        /// <param name="displayGrid">True to display the brackets at every node.</param>
         public void DrawGrid(bool displayGrid)
         {
-            for (int i = 0; i < Grid.GetUpperBound(0) + 1; i++)
+            for (int y = 0; y < Grid.GetUpperBound(1) + 1; y++)
             {
-                //if(i == 0)
-                //{
+                if (y == 0)
+                {
+                    //Draw the border. 
+                    Console.Write(" "); //The upper left corner.
+                    for (int i = 0; i < Grid.GetUpperBound(0) + 1; i++)
+                        Console.Write("---"); //Top border. Three dashes per node in the list.
+                    Console.Write(" "); //The upper right corner.
+                    Console.WriteLine(); //New line before grid is drawn.
+                }
+                for (int x = 0; x < Grid.GetUpperBound(0) + 1; x++)
+                {
+                    if (x == 0)
+                        Console.Write("|"); //Border before each row.
 
-                //}
-                for (int j = 0; j < Grid.GetUpperBound(1) + 1; j++)
-                    Console.Write(Grid[i, j].DrawPoint(displayGrid));
+                    //The grid itself.
+                    Console.Write(Grid[x, y].DrawPoint(_showGrid));
+                    Console.ResetColor(); //In case color was changed by Mouse or Cheese.
 
-                Console.WriteLine();
+                    if (x == Grid.GetUpperBound(0))
+                        Console.Write("|"); //Border after each row.
+                }
+                Console.WriteLine(); //New line before bottom border.
             }
+
+            Console.Write(" "); //Lower-left corner.
+            for (int i = 0; i < Grid.GetUpperBound(0) + 1; i++)
+                Console.Write("---"); //Bottom border. Three dashes per node.
+            Console.Write(" "); //Lower-right corner.
         }
 
+        /// <summary>
+        /// Waits for user input and checks to make sure the user hits the arrow keys. Calls ValidMove to ensure that the mouse
+        /// can move to the new area (No walls or borders)
+        /// </summary>
+        /// <returns>The key that was pressed, or "L" if not.</returns>
         public ConsoleKey getUserMove()
         {
             ConsoleKeyInfo input = Console.ReadKey();
 
+            //Only arrows are valid input.
             if (input.Key == ConsoleKey.UpArrow ||
                input.Key == ConsoleKey.DownArrow ||
                input.Key == ConsoleKey.LeftArrow ||
@@ -96,48 +153,64 @@ namespace CheeseFinder
 
         }
 
+        /// <summary>
+        /// Ensures that the mouse can move to the new location.
+        /// </summary>
+        /// <param name="input">The key that the user pressed.</param>
+        /// <returns>False if the new location is invalid.</returns>
         public bool ValidMove(ConsoleKey input)
         {
+            // Validate each potential key individually.
             switch (input)
             {
                 case ConsoleKey.UpArrow:
                     if (this.Mouse.Y > 0)
-                        return true;
+                        if(Grid[Mouse.X, Mouse.Y - 1].CanMoveTo)
+                            return true;
 
                     break;
 
                 case ConsoleKey.DownArrow:
-                    if (this.Mouse.Y < Grid.GetUpperBound(0) + 1)
-                        return true;
+                    if (this.Mouse.Y < Grid.GetUpperBound(1))
+                        if (Grid[Mouse.X, Mouse.Y + 1].CanMoveTo)
+                            return true;
 
                     break;
 
                 case ConsoleKey.LeftArrow:
                     if (this.Mouse.X > 0)
-                        return true;
+                        if (Grid[Mouse.X - 1, Mouse.Y].CanMoveTo)
+                            return true;
 
                     break;
 
                 case ConsoleKey.RightArrow:
-                    if (this.Mouse.X < Grid.GetUpperBound(0) + 1)
-                        return true;
+                    if (this.Mouse.X < Grid.GetUpperBound(0))
+                        if (Grid[Mouse.X + 1, Mouse.Y].CanMoveTo)
+                           return true;
 
                     break;
             }
 
+            //Will only get here if no arrow was pressed.
             Console.WriteLine("Not a valid move");
             return false;
         }
 
-        public bool MoveMouse(ConsoleKey input)
+        /// <summary>
+        /// Calls getUserMove to accept and validate input. Physically moves the mouse if valid.
+        /// </summary>
+        /// <returns></returns>
+        public bool MoveMouse()
         {
-            ConsoleKey userMove = getUserMove();
-            Grid[Mouse.X, Mouse.Y].Contains = PointContents.Space;
+            ConsoleKey userMove = getUserMove(); //Input is validated here.
+            Grid[Mouse.X, Mouse.Y].Contains = PointContents.Space; //Mouse likely no longer to be here after press.
 
+            //Change position if arrow is returned by getUserMove(). "L," the character returned upon incorrect
+            //input, will simply do nothing.
             switch (userMove)
             {
                 case ConsoleKey.UpArrow:
-                    Console.WriteLine("Up");
                     Mouse.Y = Mouse.Y - 1;
                     break;
 
@@ -154,27 +227,35 @@ namespace CheeseFinder
                     break;
             }
 
-            Console.WriteLine("Test");
+            //If the cheese is in the same place as the mouse, the user wins.
             if ((Mouse.X == Cheese.X) && (Mouse.Y == Cheese.Y))
                 return true;
 
-            Console.WriteLine("You move");
+            //If not, move the mouse. If L is returned, redraw in old position.
             Grid[Mouse.X, Mouse.Y].Contains = PointContents.Mouse;
             return false;
 
         }
 
-
+        /// <summary>
+        /// Play a game of CheeeseFinder.
+        /// </summary>
         public void PlayGame()
         {
-            ConsoleKeyInfo input;
+            bool foundCheese = false; //Will beset to True when cheese is found.
 
             do
             {
                 Console.Clear();
-                this.DrawGrid(true);;
-                MoveMouse(input);
-            } while (true);
+                this.DrawGrid(_showGrid);
+                foundCheese = MoveMouse(); //Returns true if mouse touches cheese.
+
+            } while (!foundCheese);
+
+            //Will only exit on victory in this version.
+            Console.Clear();
+            Console.WriteLine("You win!");
+            Console.ReadKey();
         }
     }
 
@@ -204,6 +285,10 @@ namespace CheeseFinder
         }
 
         private PointContents _contains;
+        /// <summary>
+        /// Changing this property automatically changes the character representation of this grid node, as well
+        /// as whether the user can move to this square.
+        /// </summary>
         public PointContents Contains
         {
             get { return _contains; }
@@ -285,10 +370,24 @@ namespace CheeseFinder
         /// <returns>The string if this point's contentents and the grid if drawGrid = true.</returns>
         public string DrawPoint(bool drawGrid)
         {
-            if(drawGrid)
-                return "[" + ContainsChar + "]";
+            //Change color based on the contents of the grid. The DrawGrid method will restore the white color.
+            if (this.Contains == PointContents.Mouse)
+                Console.ForegroundColor = ConsoleColor.Green;
 
-            return " " + ContainsChar + " ";
+            else if (this.Contains == PointContents.Cheese)
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+            //If we are to draw the entire grid, return all points with brackets.
+            if(drawGrid)
+                return "[" + this.ContainsChar + "]";
+
+            //If not, but this is a point that is NOT a space, return the point with brackets.
+            if(!(this.Contains == PointContents.Space))
+                return "[" + this.ContainsChar + "]";
+
+            //If it IS a space, return an empty cell. Remains "ContainsChar" in case a change is made
+            //to the representation of a space.
+            return " " + this.ContainsChar + " ";
         }
     }
 }
